871bf681eb2fbd5fe3db48af5b667cb152c41143
BrunoAlves <brunoalvesvidal20@gmail.com>
2024-11-23 14:09:01 -0300

    att cruds

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,7 +10,7 @@ def test_need_ok(client):
 
 def teste_create_user(client):
     response = client.post(
-        "/user/",
+        "/users/",
         json={
             "username": "testusername",
             "password": "password",
@@ -24,3 +24,41 @@ def teste_create_user(client):
         "email": "teste@test.com",
         "id": 1,
     }
+
+
+def teste_read_users(client):
+    response = client.get("/users/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "username": "testusername",
+                "email": "teste@test.com",
+                "id": 1,
+            }
+        ]
+    }
+
+
+def test_update_users(client):
+    response = client.put(
+        "/users/1",
+        json={
+            "password": "123",
+            "username": "testeusername2",
+            "email": "teste@test.com",
+            "id": 1,
+        },
+    )
+    assert response.json() == {
+        "username": "testeusername2",
+        "email": "teste@test.com",
+        "id": 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete("/users/1")
+
+    assert response.json() == {"message": "User deleted"}
