f127b4bef4c0f88f097dcf268c878be932bdd167
BrunoAlves <brunoalvesvidal20@gmail.com>
2025-02-17 20:10:00 -0300

    update crud/pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,47 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 from fastapi_main.app import app
-from fastapi_main.models import table_registry
+from fastapi_main.database import get_session
+from fastapi_main.models import Base
+
+DATABASE_URL = "sqlite:///:memory:"
+
+
+@pytest.fixture(scope="session")
+def engine():
+    return create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False},
+        poolclass=NullPool,
+    )
+
+
+@pytest.fixture(scope="session")
+def tables(engine):
+    Base.metadata.create_all(engine)
+    yield
+    Base.metadata.drop_all(engine)
 
 
 @pytest.fixture
-def client():
-    return TestClient(app)
+def session(engine, tables):
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    yield session
+    session.close()
 
 
 @pytest.fixture
-def session():
-    engine = create_engine("sqlite:///:memory:")
-    table_registry.metadata.create_all(engine)
+def client(session):
+    def get_session_override():
+        return session
 
-    with Session(engine) as session:
-        yield session
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
 
-    table_registry.metadata.drop_all(engine)
+        yield client
+    app.dependency_overrides.clear()
